80adb968bd79f70c4a80a217bc6bc18d0f6cb358
Stephen Hill <stephen@gatekiller.co.uk>
2013-01-07 12:46:51 +0000

    Update form.php

--- form.php
+++ form.php
@@ -4,5 +4,16 @@ namespace Stephen-Hill\Data
 {
 	class Form
 	{
+		public function __construct()
+		{
+			$this->fields = new ArrayObject();
+		}
+		
+		private $fields;
+		
+		public function addField(FormField $field)
+		{
+			$this->fields->append($field);
+		}
 	}
 }
