ff195b9a45767126ebf29451a7e321ff1e48209b
Stephen Hill <stephen@gatekiller.co.uk>
2013-01-10 18:48:44 +0000

    Adding a constructor.

--- formfield.php
+++ formfield.php
@@ -4,5 +4,16 @@ namespace Stephen-Hill\Data
 {
   class FormField
 	{
+		private $id;
+		
+		public function __construct($id)
+		{
+			if (is_string($id) === false)
+			{
+				throw new \Exception("\$id must be a string");
+			}
+			
+			$this->id = $id;
+		}
 	}
 }
